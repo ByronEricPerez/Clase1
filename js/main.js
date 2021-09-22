8c83d078de5a2a954d708d9790a69e38e3d12559
unknown <xxloboisthemanxx@gmail.com>
2021-09-22 14:39:45 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+//
+
+/* 
+
+var mascota = "Tito";
+
+let leagueTeam = "Top , Mid , Supp , ADC , Jg";
+
+let edad = 27;
+
+var numberOne = prompt ("Ingrese el primer numero");
+
+var numberTwo = prompt ("Ingrese el segundo numero");
+
+var resultado = numberOne + numberTwo
+
+console.log(resultado)
+
+
+
+let UserNum = prompt (Ingrese Edad);
+
+let UserNum = prompt (Ingrese Edad);
+
+if(console.log)
+console.log(numberUser + Welcome Raul);
+
+var userName = prompt("Ingrese usuario aqui");
+var userAge = prompt("Ingrese edad aqui");
+
+alert("Bienvenido "+userName "su edad es "+userAge)
+*/
+
+var numberFirst = prompt("Ingresar primer numero");
+var numberSecond = prompt("Ingresar segundo numero");
+var numerThird = prompt("Ingresar tercer numero");
+
+var average = (parseFloat(numberFirst)+parseFloat(numberSecond)+parseFloat(numerThird)) / 3;
+
+alert("Promedio: "+average)
